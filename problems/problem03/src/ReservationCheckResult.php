c614dd6210fd688167702ad28d7c9daf29054437
melank <masaki.vr@gmail.com>
2026-01-04 17:22:16 +0900

    problem03の解答を追加

--- problems/problem03/src/ReservationCheckResult.php
+++ problems/problem03/src/ReservationCheckResult.php
@@ -1,3 +1,11 @@
 <?php
 
 declare(strict_types=1);
+
+final readonly class ReservationCheckResult {
+
+    public function __construct(
+        public readonly bool $canReserve,
+        public readonly ?DateTimeImmutable $nextAvailableAt
+    ) {}
+}
